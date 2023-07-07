930656bacb3c0c8e80fec6e831037b15ca1781ee
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-07 14:08:34 -0700

    handleCarDelete

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,14 +1,29 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { deleteCar } from "../store"
 
 const CarList = () => {
+  const dispatch = useDispatch()
   const { list } = useSelector((state) => state.cars)
   console.log(list)
+
+  const handleCarDelete = (id) => {
+    dispatch(deleteCar(id))
+  }
   const carsList = list.map((car) => {
     return (
-      <div key={car.id}>
-        <h1>{car.name}</h1>
-        <p>{car.cost}</p>
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          type="button"
+          onClick={() => handleCarDelete(car.id)}
+          className="button is-danger"
+        >
+          X
+        </button>
       </div>
     )
   })
