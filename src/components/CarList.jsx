e21bb9875c6d6cdf091fe9e2ba0df681da40457e
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-07 21:34:46 -0700

    highlighting text for  partial matches in add car

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,16 +1,21 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { deleteCar } from "../store"
 
 const CarList = ({ list }) => {
   const dispatch = useDispatch()
+  const { name } = useSelector((state) => state.form)
 
   const handleCarDelete = (carId) => {
     dispatch(deleteCar(carId))
   }
   const carsList = list.map((car) => {
+    const partialMatch =
+      name.length && car.name.toLowerCase().includes(name.toLowerCase())
+
+    console.log("partial match for:", car.name, partialMatch)
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${partialMatch && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
